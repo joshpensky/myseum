cacf9acf9257b131f2ce91dc2c5ef0ff19976e0a
Josh Pensky <joshuapensky@gmail.com>
2020-12-21 22:48:29 -0500

    starting migration to ts

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+const App = () => (
+  <div>
+    <header>
+      <h1>Gallery</h1>
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Learn React
+      </a>
+    </header>
+  </div>
+);
+
+export default App;
