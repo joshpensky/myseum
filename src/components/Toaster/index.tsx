7dbe1301bd50b7cb2f1c87bd152a1a39ae23aa39
Josh Pensky <joshuapensky@gmail.com>
2022-02-01 17:37:49 -0500

    style museum view, style toast

--- src/components/Toaster/index.tsx
+++ src/components/Toaster/index.tsx
@@ -1,30 +1,21 @@
 import cx from 'classnames';
 import { Toaster as BaseToaster } from 'react-hot-toast';
-import Close from '@src/svgs/Close';
-import { Info } from '@src/svgs/Info';
 import styles from './toaster.module.scss';
 
 export const Toaster = () => (
   <BaseToaster
-    position="bottom-left"
+    position="top-center"
     toastOptions={{
-      className: cx(`theme--paper`, styles.toast),
+      className: styles.toast,
       duration: 6000,
+      icon: null,
       success: {
-        icon: (
-          <span className={styles.icon}>
-            <Info />
-          </span>
-        ),
+        icon: null,
       },
       error: {
-        className: cx(`theme--paper`, styles.toast, styles.toastError),
+        className: cx(styles.toast, styles.toastError),
         duration: 10000,
-        icon: (
-          <span className={cx(styles.icon, styles.iconError)}>
-            <Close />
-          </span>
-        ),
+        icon: null,
       },
     }}
   />
--- src/features/museum/MuseumView/index.tsx
+++ src/features/museum/MuseumView/index.tsx
@@ -1,10 +1,13 @@
+import { useRouter } from 'next/router';
 import dayjs from 'dayjs';
+import toast from 'react-hot-toast';
 import Button from '@src/components/Button';
 import { GalleryDto } from '@src/data/serializers/gallery.serializer';
 import { MuseumDto } from '@src/data/serializers/museum.serializer';
 import GalleryBlock from '@src/features/museum/GalleryBlock';
 import { useAuth } from '@src/providers/AuthProvider';
 import { EditIcon } from '@src/svgs/EditIcon';
+import { EmptyGalleryIllustration } from '@src/svgs/EmptyGalleryIllustration';
 import { ShareIcon } from '@src/svgs/ShareIcon';
 import styles from './museumView.module.scss';
 
@@ -15,6 +18,21 @@ export interface MuseumViewProps {
 
 export const MuseumView = ({ galleries, museum }: MuseumViewProps) => {
   const auth = useAuth();
+  const router = useRouter();
+
+  const shareLink = async () => {
+    const url = `${window.location.origin}${router.pathname}`;
+
+    if ('share' in navigator) {
+      await navigator.share({ url });
+    } else if ('clipboard' in navigator) {
+      await navigator.clipboard.writeText(url);
+      toast.success('Copied link!');
+    } else {
+      document.execCommand('copy', false, url);
+      toast.success('Copied link!');
+    }
+  };
 
   return (
     <div className={styles.page}>
@@ -30,20 +48,30 @@ export const MuseumView = ({ galleries, museum }: MuseumViewProps) => {
               Edit
             </Button>
           )}
-          <Button className={styles.actionsItem} icon={ShareIcon}>
+          <Button className={styles.actionsItem} icon={ShareIcon} onClick={() => shareLink()}>
             Share
           </Button>
         </div>
       </header>
 
       <div className={styles.main}>
-        <ul>
-          {galleries.map(gallery => (
-            <li key={gallery.id}>
-              <GalleryBlock museum={museum} gallery={gallery} />
-            </li>
-          ))}
-        </ul>
+        {!galleries.length ? (
+          <div className={styles.emptyState}>
+            <div className={styles.emptyStateIllo}>
+              <EmptyGalleryIllustration />
+            </div>
+            <p className={styles.emptyStateText}>You have no galleries.</p>
+            <Button className={styles.emptyStateAction}>Create gallery</Button>
+          </div>
+        ) : (
+          <ul className={styles.galleryList}>
+            {galleries.map(gallery => (
+              <li key={gallery.id} className={styles.galleryListItem}>
+                <GalleryBlock museum={museum} gallery={gallery} />
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
