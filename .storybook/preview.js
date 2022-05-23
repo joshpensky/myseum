44f40110f03101c42e5b58526c1d3a0b2498d2ce
Josh Pensky <joshuapensky@gmail.com>
2022-05-22 22:14:19 -0400

    configure storybook backgrounds, fix colors for components based on testing

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,28 @@
+import { ThemeProvider } from '@src/providers/ThemeProvider';
+
+const themes = [
+  {
+    name: 'paper',
+    value: '#f8f8f8',
+  },
+  {
+    name: 'ink',
+    value: '#090909',
+  },
+  {
+    name: 'mint',
+    value: '#f0f2ea',
+  },
+  {
+    name: 'rose',
+    value: '#fcf1ec',
+  },
+  {
+    name: 'navy',
+    value: '#232e3f',
+  },
+];
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -7,4 +32,23 @@ export const parameters = {
     },
   },
   layout: 'centered',
+  backgrounds: {
+    default: 'paper',
+    values: themes,
+  },
 };
+
+export const decorators = [
+  (Story, context) => {
+    const theme =
+      themes.find(theme => theme.value === context.globals.backgrounds?.value) ?? themes[0];
+
+    return (
+      <ThemeProvider theme={{ color: theme.name }}>
+        <div className={`theme--${theme.name}`}>
+          <Story />
+        </div>
+      </ThemeProvider>
+    );
+  },
+];
